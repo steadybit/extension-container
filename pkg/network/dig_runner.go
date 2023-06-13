df7466f0e85f1ac044db377e164a50bf230e6d23
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-06-13 14:37:51 +0200

    chore: update dependencies

---
+++ pkg/network/dig_runner.go
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2023 Steadybit GmbH
+
+package network
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/rs/zerolog/log"
+	"github.com/steadybit/extension-container/pkg/container/runc"
+	"github.com/steadybit/extension-container/pkg/utils"
+	"io"
+)
+
+type RuncDigRunner struct {
+	Runc runc.Runc
+	Cfg  TargetContainerConfig
+}
+
+func (d RuncDigRunner) Run(ctx context.Context, arg []string, stdin io.Reader) ([]byte, error) {
+	id := getNextContainerId()
+
+	bundle, cleanup, err := d.Runc.PrepareBundle(ctx, "sidecar.tar", id)
+	defer func() { _ = cleanup() }()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := utils.CopyFileFromProcessToBundle(bundle, d.Cfg.Pid, "/etc/resolv.conf"); err != nil {
+		log.Warn().Err(err).Msg("could not copy /etc/resolv.conf")
+	}
+
+	if err := utils.CopyFileFromProcessToBundle(bundle, d.Cfg.Pid, "/etc/hosts"); err != nil {
+		log.Warn().Err(err).Msg("could not copy /etc/hosts")
+	}
+
+	if err = runc.EditSpec(
+		bundle,
+		runc.WithHostname(fmt.Sprintf("dig-%s", id)),
+		runc.WithAnnotations(map[string]string{
+			"com.steadybit.sidecar": "true",
+		}),
+		runc.WithSelectedNamespaces(d.Cfg.Namespaces, specs.NetworkNamespace, specs.UTSNamespace),
+		runc.WithCapabilities("CAP_NET_ADMIN"),
+		runc.WithProcessArgs(append([]string{"dig"}, arg...)...),
+	); err != nil {
+		return nil, err
+	}
+
+	var outb, errb bytes.Buffer
+	err = d.Runc.Run(ctx, id, bundle, runc.IoOpts{Stdin: stdin, Stdout: &outb, Stderr: &errb})
+	defer func() { _ = d.Runc.Delete(context.Background(), id, true) }()
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", err, errb.String())
+	}
+	return outb.Bytes(), nil
+}
