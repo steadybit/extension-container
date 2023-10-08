44a84d8474a021ea7b028a84ae9cbc395ba38423
Johannes Edmeier <johannes.edmeier@steadybit.com>
2023-10-08 22:58:53 +0200

    fix: reduce discovery call interval to 30s

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ type Specification struct {
 	RuncSystemdCgroup           bool     `json:"runcSystemdCgroup" split_words:"true" required:"false"`
 	RuncDebug                   bool     `json:"runcDebug" split_words:"true" required:"false"`
 	DisableDiscoveryExcludes    bool     `required:"false" split_words:"true" default:"false"`
-	DiscoveryCallInterval       string   `json:"discoveryCallInterval" split_words:"true" required:"false" default:"1m"`
+	DiscoveryCallInterval       string   `json:"discoveryCallInterval" split_words:"true" required:"false" default:"30s"`
 	Port                        uint16   `json:"port" split_words:"true" required:"false" default:"8086"`
 	HealthPort                  uint16   `json:"healthPort" split_words:"true" required:"false" default:"8082"`
 	DiscoveryAttributesExcludes []string `json:"discoveryAttributesExcludes" split_words:"true" required:"false"`
